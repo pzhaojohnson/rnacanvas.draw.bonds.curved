fd9cd7e1f9a12ff4fee66a4efe0c1d73af2c2877
Philip Johnson <p.zhao.johnson@gmail.com>
2026-06-25 19:02:05 -0400

    Expand required nucleobase interface

--- src/CurvedBond.ts
+++ src/CurvedBond.ts
@@ -2,6 +2,16 @@ import type { Nucleobase } from './Nucleobase';
 
 import { v4 as uuidv4 } from 'uuid';
 
+import { D } from './D';
+
+import { midpoint } from '@rnacanvas/points.oopified';
+
+import { BasePadding } from './BasePadding';
+
+import { direction } from '@rnacanvas/points';
+
+import { Point } from '@rnacanvas/points.oopified';
+
 export class CurvedBond {
   /**
    * Creates and returns a new curved bond between the two bases.
@@ -31,4 +41,113 @@ export class CurvedBond {
   }
 
   constructor(readonly domNode: SVGPathElement, readonly base1: Nucleobase, readonly base2: Nucleobase) {}
+
+  /**
+   * Repositions the curved bond based on the current positions of bases 1 and 2
+   * and the cached base paddings.
+   */
+  #reposition(): void {
+    let basePadding1 = BasePadding.from(this.domNode.dataset.basePadding1);
+    let basePadding2 = BasePadding.from(this.domNode.dataset.basePadding2);
+
+    // absolutize
+    basePadding1 = this.#absolutized1(basePadding1);
+    basePadding2 = this.#absolutized2(basePadding2);
+
+    let d = D.matching(this.domNode.getAttribute('d'));
+
+    let mp = midpoint(d.startPoint, d.endPoint);
+
+    let displacements: [Point, Vector][] = d.interveningPoints.map(p => [Point.matching(p), mp.displacementTo(p)]);
+
+    // relativize displacements
+    displacements.forEach(([_, v]) => v.direction -= direction(d.startPoint, d.endPoint));
+
+    // reposition start and end points
+    d.startPoint.set(Point.matching(this.base1.centerPoint).displaced(basePadding1));
+    d.endPoint.set(Point.matching(this.base2.centerPoint).displaced(basePadding2));
+
+    // recalculate midpoint
+    mp = midpoint(d.startPoint, d.endPoint);
+
+    // absolutize displacements
+    displacements.forEach(([_, v]) => v.direction += direction(d.startPoint, d.endPoint));
+
+    // reposition intervening points
+    displacements.forEach(([p, v]) => p.set(mp.displaced(v)));
+
+    this.domNode.setAttribute('d', d.toString());
+  }
+
+  #cacheBasePaddings(): void {
+    let d = D.matching(this.domNode.getAttribute('d'));
+
+    let v1 = Point.matching(this.base1.centerPoint).displacementTo(d.startPoint);
+    let v2 = Point.matching(this.base2.centerPoint).displacementTo(d.endPoint);
+
+    let basePadding1 = new BasePadding(v1.magnitude, v1.direction);
+    let basePadding2 = new BasePadding(v2.magnitude, v2.direction);
+
+    // relativize
+    basePadding1 = this.#relativized1(basePadding1);
+    basePadding2 = this.#relativized2(basePadding2);
+
+    // cache
+    this.domNode.dataset.basePadding1 = basePadding1.toJSON();
+    this.domNode.dataset.basePadding2 = basePadding2.toJSON();
+  }
+
+  /**
+   * Returns a new base padding object with its direction absolutized to the direction from base 1 to base 2.
+   *
+   * Doesn't modify the input base padding object.
+   */
+  #absolutized1(basePadding: BasePadding): BasePadding {
+    let absolutized = BasePadding.from(basePadding.toJSON());
+
+    absolutized.direction += direction(this.base1.centerPoint, this.base2.centerPoint);
+
+    return absolutized;
+  }
+
+  /**
+   * Returns a new base padding object with its direction absolutized to the direction from base 2 to base 1.
+   *
+   * Doesn't modify the input base padding object.
+   */
+  #absolutized2(basePadding: BasePadding): BasePadding {
+    let absolutized = BasePadding.from(basePadding.toJSON());
+
+    absolutized.direction += direction(this.base2.centerPoint, this.base1.centerPoint);
+
+    return absolutized;
+  }
+
+  /**
+   * Returns a new base padding object with its direction relativized to the direction from base 1 to base 2.
+   *
+   * Doesn't modify the input base padding object.
+   */
+  #relativized1(basePadding: BasePadding): BasePadding {
+    let relativized = BasePadding.from(basePadding.toJSON());
+
+    relativized.direction -= direction(this.base1.centerPoint, this.base2.centerPoint);
+
+    return relativized;
+  }
+
+  /**
+   * Returns a new base padding object with its direction relativized to the direction from base 2 to base 1.
+   *
+   * Doesn't modify the input base padding object.
+   */
+  #relativized2(basePadding: BasePadding): BasePadding {
+    let relativized = BasePadding.from(basePadding.toJSON());
+
+    relativized.direction -= direction(this.base2.centerPoint, this.base1.centerPoint);
+
+    return relativized;
+  }
 }
+
+type Vector = ReturnType<Point['displacementTo']>;
--- src/Nucleobase.ts
+++ src/Nucleobase.ts
@@ -1,1 +1,14 @@
-export interface Nucleobase {}
+export interface Nucleobase {
+  readonly domNode: SVGTextElement;
+
+  readonly id: string;
+
+  readonly centerPoint: {
+    readonly x: number;
+    readonly y: number;
+  };
+
+  addEventListener(name: 'change', listener: () => void): void;
+
+  removeEventListener(name: 'change', listener: () => void): void;
+}
